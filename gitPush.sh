8cb2bef6412041d60a003adf438bab26c6f8e7f2
xiangang.zhu <xiangang.zhu@tcl.com>
2017-02-27 18:51:32 +0800

    add git push scripts

---
+++ gitPush.sh
@@ -0,0 +1,8 @@
+#do some change
+#git init (if no ./git file)
+#git add *modifyed files*
+#git commit -m "the description of this commit"
+#*************************************************************
+git remote rm origin
+git remote add origin https://github.com/laozhuxinlu/yii2_basic.git
+git push origin master
